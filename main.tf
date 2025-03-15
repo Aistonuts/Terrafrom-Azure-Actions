1f83e6a1065c70146e3526737a5edd60a1565d7d
Aistonuts <120518044+Aistonuts@users.noreply.github.com>
2025-03-15 12:49:49 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 # Configure the Azure provider
 terraform {
   required_providers {
-      azurerm  = {
-      source   = "hashicorp/azurerm"
-      version  = "3.90.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.90.0"
     }
   }
 }
